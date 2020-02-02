ab6fe10b0a3b10c3dfbf9feb8b92764333c665f8
aritche <11988281+aritche@users.noreply.github.com>
2020-02-03 03:41:58 +1100

    basic attempt at random sampling. Still working on it.

--- chexnet/localisation/ensembling/network_ens.py
+++ chexnet/localisation/ensembling/network_ens.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import csv
 from statistics import stdev
 import os
+from random import shuffle
 
 # Returns a dictionary mapping file name (without extension) to bounding box
 def get_truth_boxes(target_class, truth_file, original_size, new_size):
@@ -90,115 +91,148 @@ def get_top_5_predictions(mask_dir):
     return predictions
 
 
+CLASS_NAME = 'Cardiomegaly'
 csv_file = '../data/BBox_List_2017.csv'
-all_ims = '../data/cardio_images_256'
+all_ims = '../data/localisation_classes_256\\' + CLASS_NAME
 
 
-gradcam = '..\\gradcam_results\\repeat_'
-mp = '..\\mp_results\\repeat_'
+gradcam = '..\\results\\' + CLASS_NAME + '\\gradcam\\repeat_'
+mp = '..\\results\\' + CLASS_NAME +'\\mp\\repeat_'
 im_size = 224
 
-truth_boxes = get_truth_boxes('Cardiomegaly', csv_file, 1024, im_size)
+truth_boxes = get_truth_boxes(CLASS_NAME, csv_file, 1024, im_size)
 
 repeats = int(input("How many networks are you ensembling?"))
 
-t = 0.5
+gc_t = 0.5
+mp_t = 0.5
 
-predictions = []
-gradcam_boxes = []
-mp_boxes = []
-for repeat in range(repeats):
-    curr_gc_fn = gradcam + str(repeat+1)
-    curr_mp_fn = mp + str(repeat+1)
-    predictions.append(get_top_5_predictions(curr_gc_fn))
-    gradcam_boxes.append(get_predicted_boxes(curr_gc_fn, t))
-    mp_boxes.append(get_predicted_boxes(curr_mp_fn, t))
+gc_weight = 1
+mp_weight = 1
 
-im_boxes = {}
-for im_fn in gradcam_boxes[0]:
-    curr_gc_boxes = []
-    curr_mp_boxes = []
-    
-    result_box = [0, 0, 0, 0]
+#for gc_t in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+#    for mp_t in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+#        for gc_weight in [0, 0.2, 0.4, 0.6, 0.8, 1.0]:
+#            for mp_weight in [0, 0.2, 0.4, 0.6, 0.8, 1.0]:
 
-    for repeat in range(repeats):
-        curr_gc_boxes.append(gradcam_boxes[repeat][im_fn])
-        curr_mp_boxes.append(mp_boxes[repeat][im_fn])
+valid_fns = list(truth_boxes.keys())
+shuffle(valid_fns)
+valid_fns = valid_fns[:len(valid_fns)//4]
+print("Generated sample of length %d" % (len(valid_fns)//4))
 
-        result_box[0] += (gradcam_boxes[repeat][im_fn][0] + mp_boxes[repeat][im_fn][0])/(2)
-        result_box[1] += (gradcam_boxes[repeat][im_fn][1] + mp_boxes[repeat][im_fn][1])/(2)
-        result_box[2] += (gradcam_boxes[repeat][im_fn][2] + mp_boxes[repeat][im_fn][2])/(2)
-        result_box[3] += (gradcam_boxes[repeat][im_fn][3] + mp_boxes[repeat][im_fn][3])/(2)
+#for gc_t in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+#    for mp_t in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+for gc_t in [0.5]:
+    for mp_t in [0.5]:
+        for gc_weight in [0, 0.2, 0.4, 0.6, 0.8, 1.0]:
+            for mp_weight in [0, 0.2, 0.4, 0.6, 0.8, 1.0]:
+                if gc_weight == 0 and mp_weight == 0:
+                    continue
+                predictions = []
+                gradcam_boxes = []
+                mp_boxes = []
+                for repeat in range(repeats):
+                    curr_gc_fn = gradcam + str(repeat+1)
+                    curr_mp_fn = mp + str(repeat+1)
+                    predictions.append(get_top_5_predictions(curr_gc_fn))
+                    gradcam_boxes.append(get_predicted_boxes(curr_gc_fn, gc_t))
+                    mp_boxes.append(get_predicted_boxes(curr_mp_fn, mp_t))
 
-        
-    for i in range(len(result_box)):
-        result_box[i] /= repeats 
-        result_box[i] = int(result_box[i])
 
-    im_boxes[im_fn] = result_box
+                im_boxes = {}
+                for im_fn in valid_fns:
+                    curr_gc_boxes = []
+                    curr_mp_boxes = []
+                    
+                    result_box = [0, 0, 0, 0]
 
-IOUs = []
-for item in im_boxes:
-    im = cv2.imread(all_ims + '/' + item + '.png')
-    x, y, w, h = im_boxes[item]
-    im = cv2.rectangle(im, (x,y), (x+w, y+h), (255,0,0), 3)
+                    for repeat in range(repeats):
+                        curr_gc_boxes.append(gradcam_boxes[repeat][im_fn])
+                        curr_mp_boxes.append(mp_boxes[repeat][im_fn])
 
-    for repeat in range(repeats):
-        x, y, w, h = gradcam_boxes[repeat][item]
-        im = cv2.rectangle(im, (x,y), (x+w, y+h), (0,127,127), 3)
-        x, y, w, h = mp_boxes[repeat][item]
-        im = cv2.rectangle(im, (x,y), (x+w, y+h), (0,127,127), 3)
 
-    x, y, w, h = truth_boxes[item]
-    im = cv2.rectangle(im, (x,y), (x+w, y+h), (0,255,0), 3)
-    #if t == 0.5:
-        #cv2.imshow('im', im)
-        #cv2.waitKey(0)
-    IOUs.append(IOU(im_boxes[item], truth_boxes[item]))
-    #break
+                        result_box[0] += (gc_weight*gradcam_boxes[repeat][im_fn][0] + mp_weight*mp_boxes[repeat][im_fn][0])/(gc_weight+mp_weight)
+                        result_box[1] += (gc_weight*gradcam_boxes[repeat][im_fn][1] + mp_weight*mp_boxes[repeat][im_fn][1])/(gc_weight+mp_weight)
+                        result_box[2] += (gc_weight*gradcam_boxes[repeat][im_fn][2] + mp_weight*mp_boxes[repeat][im_fn][2])/(gc_weight+mp_weight)
+                        result_box[3] += (gc_weight*gradcam_boxes[repeat][im_fn][3] + mp_weight*mp_boxes[repeat][im_fn][3])/(gc_weight+mp_weight)
 
-counts = {}
-iou_thresholds = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-for item in IOUs:
-    for thresh in iou_thresholds:
-        if thresh not in counts:
-            counts[thresh] = 0 # just so everything exists in the dict
-        if (item >= thresh):
-            if thresh not in counts:
-                counts[thresh] = 1
-            else:
-                counts[thresh] += 1
-    
-# Print errors at each IOU threshold
-errors = []
-for item in iou_thresholds:
-    error = 1 - (counts[item]/len(IOUs))
-    #if item == 0.5:
-        #results.append([w1, error])
-    print("%.2f:\t%.3f" % (item, error))
-    errors.append(error)
+                        
+                    for i in range(len(result_box)):
+                        result_box[i] /= repeats 
+                        result_box[i] = int(result_box[i])
 
-# Print mean statistics
-#print("Mean IOU: %.3f" % (sum(IOUs)/len(IOUs)))
-#print("SDEV IOU: %.3f" % (stdev(IOUs)))
-sizes = []
-truth_sizes = []
-widths = []
-truth_widths = []
-heights = []
-truth_heights = []
-for item in im_boxes:
-    real_h, real_w = [im_size, im_size]
-    x, y, w, h = im_boxes[item]
-    sizes.append(float(w*h) / (real_w*real_h))
-    widths.append(float(w)/float(real_w))
-    heights.append(float(h)/float(real_h))
+                    im_boxes[im_fn] = result_box
 
-    x, y, w, h = truth_boxes[item]
-    truth_sizes.append(float(w*h) / (real_w*real_h))
-    truth_widths.append(float(w)/real_w)
-    truth_heights.append(float(h)/real_h)
-print("Area\tWidth\tHeight")
-print("%.2f\t%.2f\t%.2f" % (sum(sizes)/len(sizes), sum(widths)/len(widths), sum(heights)/len(heights)))
-print("Truth:")
-print("%.2f\t%.2f\t%.2f" % (sum(truth_sizes)/len(truth_sizes), sum(truth_widths)/len(truth_widths), sum(truth_heights)/len(truth_heights)))
+
+
+                IOUs = []
+                for item in im_boxes:
+                    # draw ensemble as blue
+                    im = cv2.imread(all_ims + '/' + item + '.png')
+                    x, y, w, h = im_boxes[item]
+                    im = cv2.rectangle(im, (x,y), (x+w, y+h), (255,0,0), 3)
+
+                    # draw all others are thin
+                    for repeat in range(repeats):
+                        x, y, w, h = gradcam_boxes[repeat][item]
+                        im = cv2.rectangle(im, (x,y), (x+w, y+h), (0,127,127), 1)
+                        x, y, w, h = mp_boxes[repeat][item]
+                        im = cv2.rectangle(im, (x,y), (x+w, y+h), (0,127,127), 1)
+
+                    # draw truth as green
+                    x, y, w, h = truth_boxes[item]
+                    im = cv2.rectangle(im, (x,y), (x+w, y+h), (0,255,0), 3)
+
+                    #if t == 0.5:
+                    #    cv2.imshow('im', im)
+                    #    cv2.waitKey(0)
+
+                    IOUs.append(IOU(im_boxes[item], truth_boxes[item]))
+                    #break
+
+                counts = {}
+                iou_thresholds = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+                for item in IOUs:
+                    for thresh in iou_thresholds:
+                        if thresh not in counts:
+                            counts[thresh] = 0 # just so everything exists in the dict
+                        if (item >= thresh):
+                            if thresh not in counts:
+                                counts[thresh] = 1
+                            else:
+                                counts[thresh] += 1
+                    
+                # Print errors at each IOU threshold
+                errors = []
+                for item in iou_thresholds:
+                    error = 1 - (counts[item]/len(IOUs))
+                    if item == 0.5:
+                        #results.append([w1, error])
+                        print("%.2f,%.2f,%.2f,%.2f:\t%.3f" % (gc_t, mp_t, gc_weight, mp_weight, error))
+                    #print("%.2f:\t%.3f" % (item, error))
+                    errors.append(error)
+
+                # Print mean statistics
+                #print("Mean IOU: %.3f" % (sum(IOUs)/len(IOUs)))
+                #print("SDEV IOU: %.3f" % (stdev(IOUs)))
+                sizes = []
+                truth_sizes = []
+                widths = []
+                truth_widths = []
+                heights = []
+                truth_heights = []
+                for item in im_boxes:
+                    real_h, real_w = [im_size, im_size]
+                    x, y, w, h = im_boxes[item]
+                    sizes.append(float(w*h) / (real_w*real_h))
+                    widths.append(float(w)/float(real_w))
+                    heights.append(float(h)/float(real_h))
+
+                    x, y, w, h = truth_boxes[item]
+                    truth_sizes.append(float(w*h) / (real_w*real_h))
+                    truth_widths.append(float(w)/real_w)
+                    truth_heights.append(float(h)/real_h)
+                #print("Area\tWidth\tHeight")
+                #print("%.2f\t%.2f\t%.2f" % (sum(sizes)/len(sizes), sum(widths)/len(widths), sum(heights)/len(heights)))
+                #print("Truth:")
+                #print("%.2f\t%.2f\t%.2f" % (sum(truth_sizes)/len(truth_sizes), sum(truth_widths)/len(truth_widths), sum(truth_heights)/len(truth_heights)))
